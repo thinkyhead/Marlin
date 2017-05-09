b4254cb21c17f2d0b2976c6bfd247216cecb4c32
Scott Lahteine <github@thinkyhead.com>
2017-05-09 12:16:16 -0500

    Character Display config

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1671,7 +1671,7 @@
 //
 // Note: Usually sold with a blue PCB.
 //
-//#define G3D_PANEL
+#define G3D_PANEL
 
 //
 // RigidBot Panel V1.0
@@ -1774,7 +1774,7 @@
 // RepRapDiscount FULL GRAPHIC Smart Controller
 // http://reprap.org/wiki/RepRapDiscount_Full_Graphic_Smart_Controller
 //
-#define REPRAP_DISCOUNT_FULL_GRAPHIC_SMART_CONTROLLER
+//#define REPRAP_DISCOUNT_FULL_GRAPHIC_SMART_CONTROLLER
 
 //
 // ReprapWorld Graphical LCD
