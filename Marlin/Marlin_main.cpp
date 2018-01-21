553c318ebb97c4c8feb33092453c17d1a32b218d
Thomas Moore <tcm0116@gmail.com>
2018-01-21 13:39:06 -0600

    Change filament fixes for delta

--- Marlin/Marlin_main.cpp
+++ Marlin/Marlin_main.cpp
@@ -6275,7 +6275,7 @@ inline void gcode_M17() {
   void do_pause_e_move(const float &length, const float &fr) {
     set_destination_from_current();
     destination[E_AXIS] += length / planner.e_factor[active_extruder];
-    buffer_line_to_destination(fr);
+    planner.buffer_line_kinematic(destination, fr, active_extruder);
     stepper.synchronize();
     set_current_from_destination();
   }
@@ -10206,6 +10206,10 @@ inline void gcode_M502() {
   inline void gcode_M701() {
     point_t park_point = NOZZLE_PARK_POINT;
 
+    // Only raise Z if the machine is homed
+    if (axis_unhomed_error())
+      park_point.z = 0;
+
     if (get_target_extruder_from_command(701)) return;
 
     // Z axis lift
@@ -10262,6 +10266,10 @@ inline void gcode_M502() {
    */
   inline void gcode_M702() {
     point_t park_point = NOZZLE_PARK_POINT;
+
+    // Only raise Z if the machine is homed
+    if (axis_unhomed_error())
+      park_point.z = 0;
 
     if (get_target_extruder_from_command(702)) return;
 
