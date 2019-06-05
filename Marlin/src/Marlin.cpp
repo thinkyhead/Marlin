1192eb73115e36516489dc10cf2825ea9e74268a
Scott Lahteine <github@thinkyhead.com>
2019-06-04 20:27:58 -0500

    Adjust formatting, etc.

--- Marlin/src/Marlin.cpp
+++ Marlin/src/Marlin.cpp
@@ -936,13 +936,10 @@ void setup() {
   // (because EEPROM code calls the UI).
   ui.init();
   ui.reset_status();
-  //In order to recover brokem custom _Bootscreen
-  //if the installes lcd is a MKS_MINI_12864
-  //The Boot Logos show process has being moved to the ui.init();
-  #if !ENABLED(MKS_MINI_12864)
-    #if HAS_SPI_LCD && ENABLED(SHOW_BOOTSCREEN)
-      ui.show_bootscreen();
-    #endif
+
+  // (For MKS_MINI_12864 the bootscreen is shown in ui.init)
+  #if HAS_SPI_LCD && ENABLED(SHOW_BOOTSCREEN) && DISABLED(MKS_MINI_12864)
+    ui.show_bootscreen();
   #endif
 
   #if ENABLED(SDIO_SUPPORT) && !PIN_EXISTS(SD_DETECT)
