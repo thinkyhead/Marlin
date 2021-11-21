ac88901be47874cba79dfd29def310842aaedbd7
DerAndere <26200979+DerAndere1@users.noreply.github.com>
2021-11-21 03:04:31 +0100

    revert erroneous deletion

--- Marlin/src/core/macros.h
+++ Marlin/src/core/macros.h
@@ -33,6 +33,15 @@
 
 #define _AXIS(A) (A##_AXIS)
 
+#define _XSTOP_  0x01
+#define _YSTOP_  0x02
+#define _ZSTOP_  0x03
+#define _ISTOP_  0x04
+#define _JSTOP_  0x05
+#define _KSTOP_  0x06
+#define _USTOP_  0x07
+#define _VSTOP_  0x08
+#define _WSTOP_  0x09
 #define _XMIN_   0x11
 #define _YMIN_   0x12
 #define _ZMIN_   0x13
