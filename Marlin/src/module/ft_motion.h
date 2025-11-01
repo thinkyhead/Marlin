96aaf13b03e16bcb808940e4131035f40e2432d5
Scott Lahteine <thinkyhead@users.noreply.github.com>
2025-10-31 19:29:33 -0500

    review initial adjustments

--- Marlin/src/module/ft_motion.h
+++ Marlin/src/module/ft_motion.h
@@ -77,7 +77,7 @@ typedef struct FTConfig {
   #endif // HAS_FTM_SHAPING
 
   #if ENABLED(FTM_SMOOTHING)
-    ft_smoothed_float_t smoothingTime;                  // Smoothing time. [s]
+    ft_smoothed_float_t smoothingTime;                    // Smoothing time. [s]
   #endif
 
   TrajectoryType trajectory_type = TrajectoryType::FTM_TRAJECTORY_TYPE; // Trajectory generator type
@@ -214,9 +214,6 @@ class FTMotion {
     static Poly6TrajectoryGenerator poly6Generator;
     static TrajectoryGenerator* currentGenerator;
     static TrajectoryType trajectoryType;
-
-    // Number of batches needed to propagate the current trajectory to the stepper.
-    static constexpr uint32_t PROP_BATCHES = 1;
 
     #if ENABLED(DISTINCT_E_FACTORS)
       static uint8_t block_extruder_axis;  // Cached extruder axis index
